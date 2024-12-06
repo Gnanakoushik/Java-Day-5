743476e1f310f5cc855fd9efcb8da93f73477427
Gnanakoushik <jpranathi04@gmail.com>
2024-12-06 23:42:17 +0530

    Add files via upload

---
+++ Patternum2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Patternum2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(j+" ");
+
+            }
+        System.out.println();
+        }
+    }
+}
