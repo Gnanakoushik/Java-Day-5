4c5ca86fe49ba8e625c6412d6f67b1b937558142
Gnanakoushik <jpranathi04@gmail.com>
2024-12-06 11:22:27 +0530

    Add files via upload

---
+++ Printnaturalnuminreverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Printnaturalnuminreverse
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        //int n=5;
+        int i=5;
+        while(i>0)
+        {
+            System.out.println(i);
+            i--;
+
+        }
+    }
+}
