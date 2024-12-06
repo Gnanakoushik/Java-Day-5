6f46be6efcd2ddbbc08298b9ee1eddb43e276e08
Gnanakoushik <jpranathi04@gmail.com>
2024-12-06 23:43:31 +0530

    Add files via upload

---
+++ Patternumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Patternumber{
+    public static void main(String aargs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=0;j<=5;j++)
+            {
+              System.out.print((char)(j+64)+" ");  
+            }
+            System.out.println();
+            
+
+        }
+
+    }
+}
