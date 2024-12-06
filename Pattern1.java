c73f23f94768bda37d65fe7758ce741dd1ddf926
Gnanakoushik <jpranathi04@gmail.com>
2024-12-06 15:18:24 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pattern1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
